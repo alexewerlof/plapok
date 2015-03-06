ece0a0620dd0eef91c949b7cc27a7e56cf7f264d
Alex Ewerlöf <hanifbbz@gmail.com>
2015-03-06 01:05:27 +0100

    trying to make the voting system work

--- index.js
+++ index.js
@@ -5,14 +5,31 @@ var io = require('socket.io')(http);
 
 app.use(express.static('www'));
 
+var names = {};
+var votes = {};
+
+function getSocketNames(sockets) {
+    return sockets.map(function (socket) {
+        return names[socket.id] || socket.id;
+    });
+}
+
 io.on('connection', function(socket){
     console.log('a user connected');
+    socket.broadcast.emit('votersChanged', getSocketNames(io.sockets.sockets));
     socket.on('disconnect', function(){
+        console.log('Voter disconnected', socket.id);
         console.log('user disconnected');
     });
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-        console.log('message: ' + msg);
+    socket.on('rename', function(name){
+        socket.broadcast.emit('votersChanged', getSocketNames(io.sockets.sockets));
+        names[socket.id] = name;
+        console.info(socket.id, 'is called', name);
+    });
+    socket.on('vote', function(voter, vote){
+        console.info('Vote received from ', voter, vote);
+        io.emit('voteReceived', voter, vote);
+        votes[socket.id] = vote;
     });
 });
 
--- www/index.js
+++ www/index.js
@@ -1,26 +1,36 @@
 angular.module('plapok', [])
+    .factory('socket', function () {
+    })
     .controller('IndexCtrl', function ($scope) {
+        var socket = io();
+        $scope.myName = '';
         $scope.amVoting = false;
         $scope.choices = [1, 2, 3, 4, 7, '?'];
-        $scope.voters = [
-            {
-                name: 'Alex',
-                vote: 2
-            },
-            {
-                name: 'Oskar',
-                vote: 3
-            },
-            {
-                name: 'Rickard',
-                vote: '?'
-            },
-            {
-                name: 'Tobias',
-                vote: '...'
+        $scope.voters = [];
+        socket.on('connect', function(){
+            $scope.id = socket.id;
+            if ($scope.myName) {
+                $scope.rename();
             }
-        ];
+            $scope.$apply();
+        });
+        socket.on('votersChanged', function(voters){
+            $scope.voters = voters;
+            $scope.$apply();
+        });
+        $scope.votes = {};
+        socket.on('voteReceived', function(voter, vote){
+            $scope.votes[voter] = vote;
+            $scope.$apply();
+        });
         $scope.startVoting = function () {
             $scope.amVoting = true;
+            socket.emit('startVoting');
+        };
+        $scope.submitVote = function (vote) {
+            socket.emit('vote', $scope.myName, vote);
+        };
+        $scope.rename = function () {
+            socket.emit('rename', $scope.myName);
         }
     });
